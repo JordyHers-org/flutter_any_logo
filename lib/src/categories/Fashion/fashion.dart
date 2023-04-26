4fb42239a6e82a513d48819f7eb5e31b4f381b1f
jordyhers <jordy925@gmail.com>
2023-04-26 22:45:08 +0200

    updated fashion.dart

--- lib/src/categories/Fashion/fashion.dart
+++ lib/src/categories/Fashion/fashion.dart
@@ -1,9 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_any_logo/src/abstract/abstract.dart';
 
+//TODO  : Implement all png images
+//TODO  : Update all list in abstract class
+//TODO  : Implement values and image builder
+
 class Fashion extends FashionInterface {
+  //balenciaga
+  static AssetImage balenciaga =
+      const AssetImage('${FashionInterface.fashionPath}balenciaga.png');
+
+  // Chanel
+  static AssetImage chanel =
+      const AssetImage('${FashionInterface.fashionPath}chanel.png');
+
+// Christian Dior
+  static AssetImage dior =
+      const AssetImage('${FashionInterface.fashionPath}dior.png');
+
+// Gucci
+  static AssetImage gucci =
+      const AssetImage('${FashionInterface.fashionPath}gucci.png');
+
+// Louis Vuitton
+  static AssetImage louisVuitton =
+      const AssetImage('${FashionInterface.fashionPath}louisVuitton.png');
+
+// Prada
+  static AssetImage prada =
+      const AssetImage('${FashionInterface.fashionPath}prada.png');
+
+// Versace
+  static AssetImage versace =
+      const AssetImage('${FashionInterface.fashionPath}versace.png');
+
+// Estée Lauder
+  static AssetImage esteeLauder =
+      const AssetImage('${FashionInterface.fashionPath}esteeLauder.png');
+
+// L'Oreal
+  static AssetImage loreal =
+      const AssetImage('${FashionInterface.fashionPath}loreal.png');
+
+//LVMH
   static AssetImage lVMH =
       const AssetImage('${FashionInterface.fashionPath}louisVuitton.png');
 
-  static AssetImage balenciaga = const AssetImage('');
+// Maybelline
+  static AssetImage maybelline =
+      const AssetImage('${FashionInterface.fashionPath}maybelline.png');
+
+// MAC
+  static AssetImage mac =
+      const AssetImage('${FashionInterface.fashionPath}mac.png');
 }
