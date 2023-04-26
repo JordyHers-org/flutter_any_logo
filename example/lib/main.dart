ab4ab11d4a99aaca8abb1b2527b1f652f797d4e2
jordyhers <jordy925@gmail.com>
2023-04-26 22:45:28 +0200

    made values global

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,109 +16,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Logos'),
-        ),
         body: Center(
           child: ListView(
             children: [
-              Image(
-                image: Nba.atlantaHawks,
-              ),
-              Image(
-                image: Nba.bostonCeltics,
-              ),
-              Image(
-                image: Nba.brooklynNets,
-              ),
-              Image(
-                image: Nba.charlotteHornets,
-              ),
-              Image(
-                image: Nba.chicagoBulls,
-              ),
-              Image(
-                image: Nba.clevelandCavaliers,
-              ),
-              Image(
-                image: Nba.dallasMavericks,
-              ),
-              Image(
-                image: Nba.denverNuggets,
-              ),
-              Image(
-                image: Nba.detroitPistons,
-              ),
-              Image(
-                image: Nba.goldenStateWarriors,
-              ),
-              Image(
-                image: Nba.houstonRockets,
-              ),
-              Image(
-                image: Nba.indianaPacers,
-              ),
-              Image(
-                image: Nba.losAngelesClippers,
-              ),
-              Image(
-                image: Nba.losAngelesLakers,
-              ),
-              Image(
-                image: Nba.memphisGrizzlies,
-              ),
-              Image(
-                image: Nba.miamiHeat,
-              ),
-              Image(
-                image: Nba.milwaukeeBucks,
-              ),
-              Image(
-                image: Nba.minnesotaTimberwolves,
-              ),
-              Image(
-                image: Nba.newOrleansPelicans,
-              ),
-              Image(
-                image: Nba.newYorkKnicks,
-              ),
-              Image(
-                image: Nba.oklahomaCityThunder,
-              ),
-              Image(
-                image: Nba.orlandoMagic,
-              ),
-              Image(
-                image: Nba.philadelphia76ers,
-              ),
-              Image(
-                image: Nba.phoenixSuns,
-              ),
-              Image(
-                image: Nba.portlandTrailBlazers,
-              ),
-              Image(
-                image: Nba.sacramentoKings,
-              ),
-              Image(
-                image: Nba.sanAntonioSpurs,
-              ),
-              Image(
-                image: Nba.torontoRaptors,
-              ),
-              Image(
-                image: Nba.utahJazz,
-              ),
-              Image(
-                image: Nba.washingtonWizards,
-              ),
-              Image(
-                image: Tech.lenovo,
-              ),
-              Image(
-                image: Fashion.lVMH,
-              ),
+              ...Nba.images,
+              ...Media.images,
             ],
           ),
         ),
