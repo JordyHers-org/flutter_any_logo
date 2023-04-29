9638879cd9e82ac9ff27886a99508e3ad9ecfc5b
jordyhers <jordy925@gmail.com>
2023-04-29 09:43:59 +0200

    updated export

--- lib/flutter_logo.dart
+++ lib/flutter_logo.dart
@@ -1,4 +1,5 @@
-export 'package:flutter_any_logo/src/categories/Fashion/fashion.dart';
-export 'package:flutter_any_logo/src/categories/Media/media.dart';
-export 'package:flutter_any_logo/src/categories/Sports/basketball.dart';
-export 'package:flutter_any_logo/src/categories/Tech/tech.dart';
+export 'package:flutter_any_logo/src/model/Fashion/fashion.dart';
+export 'package:flutter_any_logo/src/model/Media/media.dart';
+export 'package:flutter_any_logo/src/model/Sports/basketball.dart';
+export 'package:flutter_any_logo/src/model/Tech/tech.dart';
+export 'package:flutter_any_logo/src/model/sports/football.dart';
