67fd3802a801c1e71c00ac84ef9f492f4c9f042b
jordyhers <jordy925@gmail.com>
2023-04-29 09:39:52 +0200

    updated tests

---
+++ test/helpers/constants.dart
@@ -0,0 +1,9 @@
+class Const {
+  static const String basketball = './assets/basketball';
+  static const String football = './assets/football';
+  static const String euroleague = './assets/euroleague';
+  static const String food = './assets/food';
+  static const String fashion = './assets/fashion';
+  static const String media = './assets/media';
+  static const String tech = './assets/tech';
+}
