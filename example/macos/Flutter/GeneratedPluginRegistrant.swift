e37c75d3aa2f4b6ffc9df5c92437d2a6ad16c9ed
jordyhers <jordy925@gmail.com>
2023-05-02 23:03:33 +0200

    fix [ios] crash

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_any_logo
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAnyLogoPlugin.register(with: registry.registrar(forPlugin: "FlutterAnyLogoPlugin"))
+}
